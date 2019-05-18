b1296ff705313e1b1910a546f8f20ab0a8dd60fc
allanalvss <45926898+allanalvss@users.noreply.github.com>
2019-05-17 23:48:54 -0300

    Update cd4511.vhd

--- latch_decodificador/cd4511.vhd
+++ latch_decodificador/cd4511.vhd
@@ -1,28 +1,25 @@
---IFPB 07/05/2019
--- decodificador
+-- 07/05/2019
+-- Sistemas Digitais
 
 library ieee;
 use ieee.std_logic_1164.all;
 
 entity cd4511 is 
- 
- port(d:in std_logic_vector(3 downto 0);
-		n_LE,n_BL,n_LT:in std_logic;
-		disp:out std_logic_vector(6 downto 0));
+port(d: in std_logic_vector(3 downto 0);
+	  n_LE,n_BL,n_LT: in std_logic;
+	  disp:out std_logic_vector(6 downto 0));
 end cd4511;
 
-architecture decod of cd4511 is 
- 
- signal latches:std_logic_vector(3 downto 0);
- signal decoders: std_logic_vector(6 downto 0);
- signal driver:std_logic_vector(6 downto 0);
- signal mode:std_logic:='0'; -- 0 -> catodo comum 
-									  -- 1 -> anodo comum
-
+architecture CI of cd4511 is 
+signal latches: std_logic_vector(3 downto 0):= "0000";
+signal decoders: std_logic_vector(6 downto 0);
+signal driver: std_logic_vector (6 downto 0);
+signal mode: std_logic := '0';-- or '1' catodo comum 
 begin
 
-latches<= d when n_lE='0' else latches;
+latches <= d when n_BL ='0' else latches;
 
+                       --abcdefg
 decoders <= "0000000" when n_BL = '0' else
 				"1111110" when latches = "0000" else
 				"0110000" when latches = "0001" else
@@ -36,10 +33,10 @@ decoders <= "0000000" when n_BL = '0' else
 				"1111011" when latches = "1001" else
 			   "0000000";
 				
-driver<="1111111" when n_LT='0' else decoders;
+driver<= "1111111" when n_LT = '0' else decoders;
 
-laco:for i in 0 to 6 generate 
+laco : for i in 0 to 6 generate 
 disp(i)<= driver(i) xor mode;
 end generate;
 
-end decod;				
+end CI;
