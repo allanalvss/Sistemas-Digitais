48ab29b7c4866b6075fd3aacb6fb1659962e163b
allanalvss <45926898+allanalvss@users.noreply.github.com>
2019-06-02 00:15:13 -0300

    Latch D com preset e reset

---
+++ latch_decodificador/latchwhen.vhd
@@ -0,0 +1,24 @@
+
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+
+entity latchwhen is 
+port(preset,reset,enable,d:in std_logic;
+	  q:out std_logic);
+end latchwhen;
+
+
+architecture lats of latchwhen is
+signal state : std_logic:='0';
+
+begin
+state <= '0' when reset ='1' else
+         '1' when preset ='1' else 
+			d when enable = '1'  else
+			state;
+
+q<=state;
+
+end lats;
