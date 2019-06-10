32410b9c9be82d75a7ffb4384b279f53cea0067f
allanalvss <45926898+allanalvss@users.noreply.github.com>
2019-06-09 23:49:40 -0300

    correcao divisor

--- divisor/div.vhd
+++ divisor/div.vhd
@@ -4,20 +4,24 @@
 -- A e B entradas, A será dividido por B
 -- Reset/Start - reiniciar e zerar o valor de acc(acumulador)
 -- Resto -- cout o programa vai realizar a operação A-B e o resultado é direcionado para o acumulador
+-- 28/05/19
+-- divisor nao otimizado
+-- Sistemas Digitais
 
 library ieee;
 use ieee.std_logic_1164.all;
 
-entity div is
-generic(size:integer := 8);
-port(A,B:in std_logic_vector(size-1 downto 0);
+entity divisor is 
+generic(size:integer:=8);
+port(a,b:in std_logic_vector(size-1 downto 0);
 	  clk,reset:in std_logic;
 	  resto,resultado:out std_logic_vector(size-1 downto 0);
 	  ready:out std_logic);
-end div;
+end divisor;
 
 
-architecture divisao of div is 
+architecture div of divisor is 
+
 
 component subtrator is 
 port(a,b:in std_logic_vector(size-1 downto 0);
@@ -26,47 +30,52 @@ port(a,b:in std_logic_vector(size-1 downto 0);
 	  cout:out std_logic);
 end component;
 
-signal acc:std_logic_vector(size-1 downto 0):= (others => '0');
-signal w:std_logic_vector(size-1 downto 0):= (others => '0');
-signal x:std_logic_vector(size-1 downto 0):= (others => '0');
+
+
+signal acc:std_logic_vector(size-1 downto 0):=(others => '0');
+signal w:std_logic_vector(size-1 downto 0):=(others => '0');
+signal x:std_logic_vector(size-1 downto 0):=(others => '0');
 signal temp_add:std_logic_vector(size-1 downto 0);
 signal temp_sub:std_logic_vector(size-1 downto 0);
-signal add_c:std_logic;
-signal sub_c:std_logic;
+signal add_carry:std_logic;
+signal sub_carry:std_logic;
 signal status:std_logic;
 signal temp:std_logic_vector(size-1 downto 0);
+signal M_S:std_logic:='1';
+signal M_AD:std_logic:='0';
 
 
 begin
-
-temp<=(size-1 downto 1=>'0',0 =>sub_c);
-sub:subtrator port map(w,x,temp_sub,'1',sub_c);
-adder:subtrator port map(w,x,temp_add,'0',add_c);
+temp <= (0 => sub_carry,others => '0');
+sub:subtrator port map(w,x,temp_sub,'1',sub_carry);
+add:subtrator port map(w,x,temp_add,'0',add_carry);
 ready<= status;
-resto<= w ;
-resultado <= acc;
+resto<=w;
+resultado<=acc;
+
+
 
 process(reset,clk)
 
 
 begin
-if(reset ='0') then
-status <= '0';
-w <= a;
-x <= b;
-acc <= (others => '0');
-
-elsif (clk'event and clk ='0') then 
-if (sub_c = '1') then
- w <= temp_sub;
- acc <= temp_add;
-else 
+if(reset='1') then
+status <='0';
+w<= a;
+x<= b;
+acc <=(others => '0');
+elsif(clk'event and clk='0') then
+if(sub_carry='1') then
+w<=temp_sub;
+acc<= temp_add;  
+else
 status <= '1';
-
 end if;
+
 
 end if;
 
 end process;
 
-end divisao;
+
+end div;
